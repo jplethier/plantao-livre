dc943ac0b6de739d76cd73671a49b91f65e41499
Bruno Pinto <brunoferreirapinto@gmail.com>
2013-03-10 03:33:00 -0300

    adding shifts table

---
+++ migrations/20130310062007-add-shift.js
@@ -0,0 +1,22 @@
+var dbm = require('db-migrate');
+var type = dbm.dataType;
+
+exports.up = function(db, callback) {
+  db.createTable('shifts', {
+    id: { type: 'int', primaryKey: true, autoIncrement: true },
+
+    city:        'string',
+    contact:     'string',
+    endDate:     'datetime',
+    phoneNumber: 'string',
+    place:       'string',
+    remark:      'text',
+    specialty:   'string',
+    startDate:   'datetime',
+    state:       'string'
+  }, callback);
+};
+
+exports.down = function(db, callback) {
+  db.dropTable('shifts', callback);
+};
